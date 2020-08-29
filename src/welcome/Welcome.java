9daf90ffb9294f58ecadf756cbb145e052e23145
SahishnuTiwari88 <sarveshtiwari88089@gmail.com>
2020-08-29 12:37:35 +0530

    A welcome program

---
+++ src/welcome/Welcome.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package welcome;
+
+/**
+ *
+ * @author Sahishnu
+ */
+public class Welcome {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hi...\n I am Sahishnu Tiwari and i welcome you a lot to my Github Account.\n Thank you");
+    }
+    
+}
